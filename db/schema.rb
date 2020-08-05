4cc4d503ac2aa578cfe8ca35d39287c9753c1e80
The-Gentleman <zraja4402@gmail.com>
2020-08-05 11:32:02 -0400

    created book  model + migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200805142917) do
+ActiveRecord::Schema.define(version: 20200805152729) do
+
+  create_table "books", force: :cascade do |t|
+    t.string  "title"
+    t.string  "author"
+    t.text    "description"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
