768a0b51651b883e9ec7fc55517889a0d46672f2
The-Gentleman <zraja4402@gmail.com>
2020-08-07 14:12:09 -0400

    added books/new form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,8 +5,17 @@ class BooksController < ApplicationController
         erb :'/books/index'
     end 
 
+    get '/books/new' do 
+        @books = Book.all
+        erb :'/books/new'
+    end 
+
     get '/books/:id' do 
         @book = Book.find_by_id(params[:id])
         erb :'/books/show'
+    end 
+
+    post '/books' do 
+        title = params[:title]
     end 
 end 
